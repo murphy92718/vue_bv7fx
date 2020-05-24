5adf5a84b5c34b33a0c490acbf6bfd4c186a02dc
刘硕 <liushuo@liushuodeMacBook-Pro.local>
2020-05-24 17:58:27 +0800

    创建项目

--- src/components/Login/index.vue
+++ src/components/Login/index.vue
@@ -1,15 +1,20 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
+  <div id="login">
+    <el-button type="primary">13132</el-button>
   </div>
 </template>
 <script>
 export default {
-  name: 'app',
+  name: 'login',
   created() {},
   data: () => ({}),
   methods: {}
 }
 </script>
 <style lang="scss" scoped>
+#login {
+  margin: 0;
+  height: 100vh;
+  background-color: #384a5d;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 import router from './router'
+import store from './store'
+import './plugins/element'
+import './styles/main'
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   router,
   render: h => h(App)
 }).$mount('#app')
